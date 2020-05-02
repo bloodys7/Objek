1ad3653acd123d7b7891d2f7c88ab53d4f396656
dhafinharish <DHaris454@gmail.com>
2020-05-02 15:44:49 +0800

    worm

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //instance
+        Worm player1 = new Worm();
+        Worm player2 = new Worm();
+
+        player1.userName = "Bejo";
+        player1.score = 500;
+        player1.length = 3.5;
+
+        player2.userName = "Coco";
+        player2.score = 200;
+        player2.length = 1.5;
+
+        player1.eatSnack();
+
+        System.out.println(player1.score);
+        player1.eatOthers(player2);
+        System.out.println(player1.score);
+
+
+    }
+}
---
+++ src/Round.java
@@ -0,0 +1,9 @@
+public class Round {
+
+    int roundTime;
+    Worm winner;
+    Worm player1;
+    Worm player2;
+    Worm player3;
+    Worm player4;
+}
